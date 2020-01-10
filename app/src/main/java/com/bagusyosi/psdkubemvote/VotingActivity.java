955a27e0f002c5b599cdf9e29a055878ec59e2e7
yosibagus <yosibagusdsd@gmail.com>
2020-01-11 05:37:54 +0700

    menambahkan layout

---
+++ app/src/main/java/com/bagusyosi/psdkubemvote/VotingActivity.java
@@ -0,0 +1,14 @@
+package com.bagusyosi.psdkubemvote;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VotingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
