7111690ce29285e4413dd1a071534916559ce4ae
yosibagus <yosibagusdsd@gmail.com>
2020-01-10 18:11:57 +0700

    create project

---
+++ app/src/main/java/com/bagusyosi/psdkubemvote/MainActivity.java
@@ -0,0 +1,18 @@
+package com.bagusyosi.psdkubemvote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+    }
+}
